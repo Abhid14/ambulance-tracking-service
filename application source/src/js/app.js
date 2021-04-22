2c21e4cdbd209484aa113764d350c0758752b0d5
Abhishek Das <abhid14@outlook.com>
2021-04-22 12:24:36 +0530

    beta

--- application source/src/js/app.js
+++ application source/src/js/app.js
@@ -1,4 +1,6 @@
 import $ from "dom7";
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
 import Framework7 from "framework7/bundle";
 
 // Import F7 Styles
@@ -7,6 +9,7 @@ import "framework7/framework7-bundle.css";
 // Import Icons and App Custom Styles
 import "../css/icons.css";
 import "../css/app.css";
+
 
 // Import Routes
 import routes from "./routes.js";
