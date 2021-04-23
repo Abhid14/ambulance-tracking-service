30c736e884e8e0570da85287f7ae8234c095c2ba
Abhishek Das <abhid14@outlook.com>
2021-04-23 16:27:41 +0530

    stable

--- application source/src/js/app.js
+++ application source/src/js/app.js
@@ -14,6 +14,11 @@ import "../css/app.css";
 import routes from "./routes.js";
 // Import main app component
 import App from "../app.f7.html";
+import firebase from "firebase/app";
+import "firebase/analytics";
+import "firebase/auth";
+import "firebase/firestore";
+
 
 var app = new Framework7({
   name: "Ambulance Tracker", // App name
@@ -27,3 +32,14 @@ var app = new Framework7({
     path: "/service-worker.js",
   },
 });
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyD8dDiGzBMWw2JRAqdfLnHILQA0XHBFBFU",
+  authDomain: "ambulancetrackerweb.firebaseapp.com",
+  databaseURL: "https://ambulancetrackerweb-default-rtdb.firebaseio.com",
+  projectId: "ambulancetrackerweb",
+  storageBucket: "ambulancetrackerweb.appspot.com",
+  messagingSenderId: "35818978401",
+  appId: "1:35818978401:web:882a658ddba4816ec6fe20",
+  measurementId: "G-C3M80W1F3Y"
+};
